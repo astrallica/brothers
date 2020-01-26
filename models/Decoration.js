0ac59e4d9180b82b8e5916cb2b18364cec6e6343
astrallica <pigrabbit86@gmail.com>
2020-01-26 12:37:57 +0900

    debug some errors

--- models/Decoration.js
+++ models/Decoration.js
@@ -3,10 +3,22 @@ const User = require("./User")
 const Balloon = require("./Balloon")
 
 const StickerSchema = new mongoose.Sechma({
-  stickerId: String,
+  stickerId: { type: String, default: "0" },
+  positionX: { type: Number, default: 0 },
+  positionY: { type: Number, default: 0 }
+})
+
+const BalloonSchema = new mongoose.Sechma({
+  balloonId: { type: String, defalut: "0" },
+  positionX: { type: Number, default: 0 },
+  positionY: { type: Number, default: 0 },  
+  scaleX: { type: Number, default: 0 },
+  scaleY: { type: Number, default: 0 }
+})
+
 const DecorationSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true },
-  BalloonId: { type: mongoose.Schema.Types.ObjectId, ref: Balloon, required: true },
+  Balloon: { type: mongoose.Schema.Types.ObjectId, ref: Balloon, required: true },
   balloonText: { type: String, defalut: "" },
   sitckers: [ 
   hairId: { type: String, default: "0" },
